afa803e973c82c405938355a7f0e20b6ee90a908
Stromschlag6 <129942548+Stromschlag6@users.noreply.github.com>
2024-12-04 13:07:35 +0100

    homepage background done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,17 +2,16 @@ import React from 'react';
 
 function HomePage() {
   return (
-    <div className="flex flex-col items-center w-screen h-screen py-2 px-3 bg-red-300">
-      <div className="flex flex-row py-3 px-2 h-1/4 bg-white text-black w-full">
-        <div className='h-full w-1/3 p-1'>
-          <img src='/images/Logo_finished.png' alt="Waldlogo" className="h-40 w-auto object-cover"/>
+    <div className="flex flex-col items-center w-screen h-screen py-2 px-3 bg-cover" style={{ backgroundImage: `url('/images/wald_hintergrundbild.jpg')` }}>
+      <div className="flex flex-row py-3 px-2 h-1/4 text-black w-full">
+        <div className='flex flex-col h-full w-1/3 p-1 justify-center'>
+          <img src='/images/Logo_finished.png' alt="Waldlogo" className="object-cover w-1/3 bg-opacity-20"/>
+          <div className='w-2/3'/>
         </div>
-        <div className='h-full w-2/3 p-4'>
-          <h1>später Bild von der Umgebung</h1>
-        </div>  
+        <div className='h-full w-2/3 p-4'/>
       </div>
-      <div className="flex flex-col h-3/4 w-full bg-white items-center">
-        <nav className="flex flex-row w-full justify-center bg-green-500 border-black border-2 text-white">
+      <div className="flex flex-col h-3/4 w-full items-center">
+        <nav className="flex flex-row h-1/4 w-full justify-center bg-green-700 border-black border-2 text-white">
           <a href="#ueber" className="py-2 px-6 w-1/4 hover:border-b-2 hover:border-white hover:bg-green-600 transition-all duration-100">
             Home
           </a>
@@ -26,12 +25,10 @@ function HomePage() {
             Sehenswürdigkeiten und Nutzung
           </a>
         </nav>
-        <div className='flex flex-row w-full bg-white items-center'>
-          <div className='h-full w-1/2'>
-            <img src='/images/wald_hintergrundbild.jpg' alt='Platzhalter für home' className=''/>
+        <div className='flex flex-row h-3/4 w-full bg-white items-center'>
+          <div className='h-full w-1/2 m-10 bg-white'>
           </div>
-          <div className='h-full w-1/2'>
-            <p></p>
+          <div className='h-full w-1/2 m-10 bg-white'>
           </div>
         </div>
       </div>
